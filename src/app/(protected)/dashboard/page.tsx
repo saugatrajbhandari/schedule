7c831aadb3ea74ea3b15044db1e9275eaf9b9fce
Saugat Rajbhandari <saugatrajbhandari47@gmail.com>
2024-12-19 22:36:09 +0545

    integrate clerk

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard() {
+  return <div className="container mx-auto">Dashboard</div>;
+}
+
+export default Dashboard;
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,45 @@
+"use client";
+
 import React from "react";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { BadgeCheck } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 function Header() {
+  const { push } = useRouter();
+
   return (
     <nav className="flex p-4 items-center justify-between">
-      <h1 className="font-semibold text-3xl">Schedule</h1>
-      <div className="flex gap-2">
-        <Button>Create an Event</Button>
+      <Link href={"/"} className="font-semibold text-3xl">
+        Schedule
+      </Link>
+      <div className="flex gap-4">
+        <Link
+          className={buttonVariants({ variant: "default" })}
+          href={"/create-event"}
+        >
+          Create Event
+        </Link>
+
+        <SignedOut>
+          <SignInButton forceRedirectUrl={"/dashboard"}>
+            <Button variant={"secondary"}>SignIn</Button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Event"
+                labelIcon={<BadgeCheck size={16} />}
+                onClick={() => push("/events")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </SignedIn>
       </div>
     </nav>
   );
